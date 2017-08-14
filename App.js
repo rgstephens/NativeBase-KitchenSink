dfca0a125410a255b557945d4365d54e6d15e9b1
Greg Stephens <greg@udon.org>
2017-08-13 21:45:46 -0700

    Expo updates

--- App.js
+++ App.js
@@ -9,7 +9,23 @@ export default class App1 extends React.Component {
     };
   }
 
+  async componentWillMount() {
+    console.log('componentWillMount');
+    await
+    Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      'Ionicons': require('@expo/vector-icons/fonts/Ionicons.ttf'),
+    });
+    console.log('componentWillMount, back from loadAsync');
+    this.setState({isReady: true});
+  }
+
   render() {
+    if (!this.state.isReady) {
+      return <Expo.AppLoading />;
+    }
+
     return <App />;
   }
 }
